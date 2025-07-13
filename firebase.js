d07a1b5f4be437bc45612d1ab3dcb23d6a320a91
JUNSEOKLEE <84374656+yun12154@users.noreply.github.com>
2025-07-13 14:59:46 +0900

    fix: add db export

--- firebase.js
+++ firebase.js
@@ -4,10 +4,16 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  // 🔑 너 프로젝트에 맞는 config
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
