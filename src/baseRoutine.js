bcf0b1445e1f5899020fd79f9b3b425ae831fead
JUNSEOKLEE <84374656+yun12154@users.noreply.github.com>
2025-07-13 14:43:38 +0900

    동적 조합 루틴으로 리팩토링

---
+++ src/baseRoutine.js
@@ -0,0 +1,45 @@
+export const baseRoutine = {
+  "파워리프팅": [
+    { day: "월요일", workouts: ["스쿼트", "벤치프레스", "데드리프트"] },
+    { day: "화요일", workouts: ["코어", "휴식"] },
+    { day: "수요일", workouts: ["벤치프레스", "데드리프트"] },
+    { day: "목요일", workouts: ["휴식"] },
+    { day: "금요일", workouts: ["스쿼트", "벤치프레스"] },
+    { day: "토요일", workouts: ["테크닉 연습", "스트레칭"] },
+    { day: "일요일", workouts: ["휴식"] }
+  ],
+  "맨몸운동": [
+    { day: "월요일", workouts: ["푸쉬업", "풀업"] },
+    { day: "화요일", workouts: ["코어", "걷기"] },
+    { day: "수요일", workouts: ["푸쉬업 변형", "풀업 변형"] },
+    { day: "목요일", workouts: ["휴식"] },
+    { day: "금요일", workouts: ["런지", "코어"] },
+    { day: "토요일", workouts: ["스트레칭"] },
+    { day: "일요일", workouts: ["휴식"] }
+  ],
+  "크로스핏": [
+    { day: "월요일", workouts: ["WOD", "버피"] },
+    { day: "화요일", workouts: ["코어", "로잉머신"] },
+    { day: "수요일", workouts: ["WOD", "스쿼트"] },
+    { day: "목요일", workouts: ["휴식"] },
+    { day: "금요일", workouts: ["WOD", "버피"] },
+    { day: "토요일", workouts: ["스트레칭"] },
+    { day: "일요일", workouts: ["휴식"] }
+  ],
+  "복싱": [
+    { day: "월요일", workouts: ["스텝 연습", "쉐도우복싱"] },
+    { day: "화요일", workouts: ["코어", "풋워크"] },
+    { day: "수요일", workouts: ["샌드백", "쉐도우복싱"] },
+    { day: "목요일", workouts: ["휴식"] },
+    { day: "금요일", workouts: ["스텝", "코어"] },
+    { day: "토요일", workouts: ["스트레칭"] },
+    { day: "일요일", workouts: ["휴식"] }
+  ]
+};
+
+export const partBonus = {
+  "가슴": ["딥스", "플라이"],
+  "어깨": ["사이드레터럴", "업라이트로우"],
+  "등": ["풀업 변형", "바벨로우"],
+  "하체": ["런지", "레그프레스"]
+};
